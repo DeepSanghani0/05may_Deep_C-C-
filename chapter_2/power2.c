e043c37ddcb914e2df694309346d1521a2d56f7f
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-05-30 00:51:36 +0530

    add

--- chapter_2/power2.c
+++ chapter_2/power2.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 #include<math.h>
-int main(){
-
+int main()
+{
     int x = 2;
     int y = 3;
     int power = pow(x,y);
-    
-printf("%d",power);
+    printf("%d",power);
     return 0;
 }
