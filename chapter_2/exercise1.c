dd85eee86963a571698b0c622ee0facf8d656247
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-06-01 08:13:44 +0530

    add

--- chapter_2/exercise1.c
+++ chapter_2/exercise1.c
@@ -5,16 +5,10 @@ int main()
 {
     int issunday = 1;
     int itssnowing = 1;
-
 printf("%d \n", issunday && itssnowing);
 
   int itsmonday = 1;
   int itsraining = 1;
-
   printf("%d",itsmonday || itsraining);
-
-
-
-
  return 0;
 }
