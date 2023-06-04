50ea073ab00732c4a9918c4465d3c9a207677e0c
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-06-04 22:18:52 +0530

    add

--- c practice/chapter 2/table.c
+++ c practice/chapter 2/table.c
@@ -3,14 +3,9 @@ int main(){
     int n;
     printf("enter the value of n: ");
     scanf("%d",&n);
-    for (int i = 1; i <=10; i++)
-    {
-        printf("%d %d %d \n",n,i,i*n);
-    }
-    
-
-
-
-
+        for (int i = 1; i <=10; i++)
+        {
+            printf("%d %d %d \n",n,i,i*n);
+        }
     return 0;
 }
