ff70e3f32d25f629695682cb88d2526869c23f83
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-07-04 15:44:42 +0530

    add

---
+++ c practice/array/araay1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int i;
+    int array1[6];
+    for (int i = 0; i < 5; i++)
+    {
+    printf("enter the value of array[%d]",i);
+    scanf("%d",&array1[i]);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        printf("array[%d]=%d \n",i,array1[i]);
+    }
+    
+    return 0;
+}
