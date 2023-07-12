a82836cd368f9e551c96c316b00986b60d67049d
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-07-12 10:01:06 +0530

    add

---
+++ c practice/function/string.c
@@ -0,0 +1,15 @@
+// #include<stdio.h>
+// #include<string.h>
+// int main(){
+//     char name[20];
+//     scanf("%s",&name[20]);
+//     printf("your name is %s",name[20]);
+//     return 0;
+// }
+#include<stdio.h>  
+#include <string.h>    
+int main(){    
+   char ch2[11]="javatpoint";        
+   printf("String Literal Value is: %s\n", ch2);    
+ return 0;    
+}    
