5e7d3e8b694626fd79f5cec1f6c77f7c6ec4ecdb
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-07-18 16:31:58 +0530

    add

--- c practice/function/file.c
+++ c practice/function/file.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
+ int no,i,multiply;
+ char ch;
 int main(){
     FILE *table;
-    int no,i,multiply;
+   
     printf("enter no.: ");
     scanf("%d",&no);
     for (int i = 1; i <= 10; i++)
@@ -13,11 +15,20 @@ int main(){
     fprintf(table,"%d %d %d \n",no,i,multiply);
     fclose(table);
     }
-    table=fopen("table.txt","a");
-    fscanf(table,"%d",&multiply);
-    fprintf(table,"%d %d %d",no,i,multiply);
-    // printf("%d %d %d \n",no,i,multiply);
+    
+    //read data from file
+    table = fopen("table.txt","r");
+    ch = fgetc(table);
+    // fscanf(table,"%c",&ch);
 
+    do
+    {
+        ch = fgetc(table);
+        printf("%c",ch);
+    } while (ch != EOF);
+    
+    printf("\n");
+    fclose(table);
     }
     return 0;
 }
